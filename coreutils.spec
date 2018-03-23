stewartsmith/fedora-rpms-coreutils

commit e9599c8226f2b56f4f4e9c69d6159135eea65f91

Remove /bin/* Provides
UsrMove happened more than 10 Fedora releases ago.

Signed-off-by: Igor Gnatenko <ignatenkobrain@fedoraproject.org>

coreutils.spec
@@ -1,7 +1,7 @@
 Summary: A set of basic GNU tools commonly used in shell scripts
 Name:    coreutils
 Version: 8.29
-Release: 4%{?dist}
+Release: 5%{?dist}
 License: GPLv3+
 Group:   System Environment/Base
 Url:     https://www.gnu.org/software/coreutils/
@@ -69,37 +69,6 @@ Patch950: coreutils-selinux.patch
 Conflicts: filesystem < 3
 # To avoid clobbering installs
 Conflicts: coreutils-single
-Provides: /bin/basename
-Provides: /bin/cat
-Provides: /bin/chgrp
-Provides: /bin/chmod
-Provides: /bin/chown
-Provides: /bin/cp
-Provides: /bin/cut
-Provides: /bin/date
-Provides: /bin/dd
-Provides: /bin/df
-Provides: /bin/echo
-Provides: /bin/env
-Provides: /bin/false
-Provides: /bin/ln
-Provides: /bin/ls
-Provides: /bin/mkdir
-Provides: /bin/mknod
-Provides: /bin/mktemp
-Provides: /bin/mv
-Provides: /bin/nice
-Provides: /bin/pwd
-Provides: /bin/readlink
-Provides: /bin/rm
-Provides: /bin/rmdir
-Provides: /bin/sleep
-Provides: /bin/sort
-Provides: /bin/stty
-Provides: /bin/sync
-Provides: /bin/touch
-Provides: /bin/true
-Provides: /bin/uname
 
 BuildRequires: attr
 BuildRequires: autoconf
@@ -281,6 +250,9 @@ fi
 %license COPYING
 
 %changelog
+* Mon Feb 26 2018 Igor Gnatenko <ignatenkobrain@fedoraproject.org> - 8.29-5
+- Remove /bin/* Provides
+
 * Mon Feb 19 2018 Kamil Dudka <kdudka@redhat.com> - 8.29-4
 - add explicit BR for the gcc compiler
 

commit 36ef66d59d08eb434daa445f484660b03170628b

make it possible to install the latest available Adobe Reader RPM for Linux
This was recently broken by e9599c8226f2b56f4f4e9c69d6159135eea65f91.

coreutils.spec
@@ -1,7 +1,7 @@
 Summary: A set of basic GNU tools commonly used in shell scripts
 Name:    coreutils
 Version: 8.29
-Release: 8%{?dist}
+Release: 9%{?dist}
 License: GPLv3+
 Group:   System Environment/Base
 Url:     https://www.gnu.org/software/coreutils/
@@ -102,6 +102,14 @@ Provides: bundled(gnulib)
 Provides: coreutils-full = %{version}-%{release}
 Obsoletes: %{name} < 8.24-100
 
+# make it possible to install the latest available Adobe Reader RPM for Linux
+Provides: /bin/cat
+Provides: /bin/chmod
+Provides: /bin/echo
+Provides: /bin/ln
+Provides: /bin/touch
+Provides: /bin/rm
+
 %description
 These are the GNU core utilities.  This package is the combination of
 the old GNU fileutils, sh-utils, and textutils packages.
@@ -116,6 +124,15 @@ Conflicts: coreutils < 8.24-100
 # http://rpm.org/ticket/874 so use RemovePathPostfixes
 # (new in rpm 4.13) to support separate file sets.
 RemovePathPostfixes: .single
+
+# make it possible to install the latest available Adobe Reader RPM for Linux
+Provides: /bin/cat
+Provides: /bin/chmod
+Provides: /bin/echo
+Provides: /bin/ln
+Provides: /bin/touch
+Provides: /bin/rm
+
 %description single
 These are the GNU core utilities,
 packaged as a single multicall binary.
@@ -257,6 +274,9 @@ fi
 %license COPYING
 
 %changelog
+* Fri Mar 23 2018 Kamil Dudka <kdudka@redhat.com> - 8.29-9
+- make it possible to install the latest available Adobe Reader RPM for Linux
+
 * Mon Mar 19 2018 Kamil Dudka <kdudka@redhat.com> - 8.29-8
 - drop BR for bison, which is not used during the build
 